8b742a13ae23a05940bc36c8fef7873a3bae1505
Shailesh Alva <Shailesh Alva>
2016-08-23 23:50:24 -0700

    removed download code

--- main.js
+++ main.js
@@ -78,6 +78,7 @@ function animate(){
          var binary_gif = encoder.stream().getData();
          data_url = 'data:image/gif;base64,'+encode64(binary_gif); 
          document.getElementById("startbutton").disabled = false;
+         document.getElementById("spinnerDiv").style.visibility = 'visible'; 
          window.location.href= data_url;
     }
 }
