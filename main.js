830ff258202671f60820808117ef227764e0a957
Shailesh Alva <Shailesh Alva>
2016-08-23 22:56:49 -0700

    GIF encoding

--- main.js
+++ main.js
@@ -6,9 +6,106 @@ context = canvas.getContext('2d');
 
 var backgroundImg = new Image();
 backgroundImg.src = "background.gif";
+backgroundImg.crossOrigin = "Anonymous";
 backgroundImg.onload = function () {
     context.drawImage(backgroundImg, 0, 0);
 }
 
 var backgroundImgLight = new Image();
 backgroundImgLight.src = "background2.jpg";
+backgroundImgLight.crossOrigin = "Anonymous";
+
+var nextCharIndex = 0;
+var userText = '';
+var pendingFrames = 0;
+var data_url;
+
+// Intialize GIF encoder
+var encoder = new GIFEncoder();
+encoder.setRepeat(0);//0  -> loop forever
+encoder.setDelay(500); //go to next frame every n milliseconds
+
+
+
+/* Handles the start button click*/
+function onStartClick(){
+    userText = document.getElementById('inputlg').value;
+    if (userText != null){
+         if (userText == ''){
+             userText = "RUN";
+         }
+
+         userText = userText.replace(/[^a-zA-Z]+/g, '');
+         userText = userText.toLowerCase();
+         pendingFrames = userText.length * 2;
+         nextCharIndex = 0;
+
+         // start the rendering
+         if (pendingFrames > 0){
+             document.getElementById("startbutton").disabled = true;
+
+             // start encoding and animation
+             encoder.start();
+             animate();
+         }
+    }
+}
+
+function animate(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (pendingFrames % 2 == 0){
+        context.drawImage(backgroundImgLight, 0, 0);
+
+        var letterIndex = userText.charCodeAt(nextCharIndex) - 97 /* ASCII value of letter A */; 
+        drawLight(xCords[letterIndex], yCords[letterIndex], 30, context);
+        nextCharIndex ++;
+    }else{
+        context.drawImage(backgroundImg, 0, 0); 
+    }
+
+    pendingFrames--;
+
+    encoder.addFrame(context);
+    
+    if (pendingFrames > 0){
+        // request new frame
+        setTimeout(function() {
+            animate();
+        }, 600);
+    }else{
+         encoder.finish();
+         var binary_gif = encoder.stream().getData();
+         data_url = 'data:image/gif;base64,'+encode64(binary_gif); 
+         document.getElementById("startbutton").disabled = false;
+    }
+}
+
+function drawLight(x, y, radius, ctx) {
+    ctx.save();
+    ctx.globalCompositeOperation = 'lighter';
+    var radialGradient = ctx.createRadialGradient(x, y, 0, x, y, radius);
+    radialGradient.addColorStop(0.0, '#BB9');
+    radialGradient.addColorStop(0.2 , '#AA8');
+    radialGradient.addColorStop(0.7 , '#330');
+    radialGradient.addColorStop(0.90, '#110');
+    radialGradient.addColorStop(1, '#000');
+    ctx.fillStyle = radialGradient;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.restore();
+} 
+
+function downloadGIF() {
+  var element = document.createElement('a');
+  element.setAttribute('href', data_url);
+  element.setAttribute('download', userText + ".gif");
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}  
